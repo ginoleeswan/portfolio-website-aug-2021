9405c56bfa903feef60867de45739d350ef853a3
Gino Swanepoel <ginoleemusic@live.com>
2021-08-15 04:06:45 +0200

    added background circles

---
+++ src/components/circles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Circles = () => {
+  return (
+    <div className="bg-circles">
+      <div className="circle-1"></div>
+      <div className="circle-2"></div>
+      <div className="circle-3"></div>
+      <div className="circle-4"></div>
+    </div>
+  );
+};
+
+export default Circles;
