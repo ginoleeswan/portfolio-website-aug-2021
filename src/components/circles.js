9e186861d6b31534c445a2d6716f2304646c2fe6
Gino Swanepoel <ginoleemusic@live.com>
2021-08-15 21:36:37 +0200

    changed circle colors

--- src/components/circles.js
+++ src/components/circles.js
@@ -5,7 +5,7 @@ const Circles = () => {
     <>
       <div className="bg-circles">
         <div className="circle-1"></div>
-        <div className="circle-2"></div>
+        {/* <div className="circle-2"></div> */}
         <div className="circle-3"></div>
         <div className="circle-4"></div>
       </div>
